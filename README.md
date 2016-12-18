185c0c52a0b54c7318509cf970005ea825a632c5
Erik Aker <eraker@gmail.com>
2016-12-18 15:40:26 -0800

    modify readme and add ga

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ personal interest, so it may never be finished, but it's pretty fun to work on.
 
 ## ToDo
 
-- Make no-js version of site and redirect if js disabled.
+- Write some posts.
 - Make Admin/CMS actually useable for adding/editing posts/series.
 - Fix date-inputter which formats and frustrates editing attempts.
 - Implement media associated with posts.
+- Make no-js version of site and redirect if js disabled.
 - Add "prev/next" links for regular posts.
 - Build User-add, User-edit admin stuff.
 - Add projects section of site.
--- src/Html/Home.hs
+++ src/Html/Home.hs
@@ -4,6 +4,7 @@
 
 module Html.Home where
 
+import qualified Data.Text                   as T
 import           Text.Blaze.Html5            as H
 import           Text.Blaze.Html5.Attributes as A
 
@@ -51,5 +52,14 @@ pageSkeleton content = do
 
 pageFooter :: H.Html
 pageFooter = H.footer ! A.id "page-footer" $
-  H.div ! A.class_ "row" $
+  H.div ! A.class_ "row" $ do
     H.p "Copyright (c) 2016 Erik Aker"
+    H.script $ H.text ga
+
+ga :: T.Text
+ga = T.unlines ["(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"
+              , "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"
+              , "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"
+              , "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');"
+              , "ga('create', 'UA-89190801-1', 'auto');"
+              , "ga('send', 'pageview');"]
