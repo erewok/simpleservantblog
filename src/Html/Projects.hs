06e31ae8ecf768c0a8cb97579f65af5f1725eeae
Erik Aker <eraker@gmail.com>
2017-02-17 17:53:13 -0800

    Fix docker-compose

--- src/Html/Projects.hs
+++ src/Html/Projects.hs
@@ -1,4 +1,12 @@
 module Html.Projects where
 
+import           Control.Monad.Except
+import qualified Data.Text                   as T
+import           Text.Blaze.Html5            as H
+import           Text.Blaze.Html5.Attributes as A
+
+import           Servant
+import           Servant.HTML.Blaze
+
 
 projectPage = undefined
